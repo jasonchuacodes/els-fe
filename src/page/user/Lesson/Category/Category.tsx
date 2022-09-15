52ccbd28ea9b7a91345edd5cfbfa8615f8e58485
Jason Clyde Chua <sirj@Jasons-MacBook-Air.local>
2022-09-15 15:10:37 +0800

    [ELS-006] modified quiz pagination

--- src/page/user/Lesson/Category/Category.tsx
+++ src/page/user/Lesson/Category/Category.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import LessonApi from '../../../../api/LessonApi';
 import { useNavigate } from 'react-router-dom';
-import { LessonContext, LessonContextType } from '../../../../context/LessonContext';
+import {
+  LessonContext,
+  LessonContextType,
+} from '../../../../context/LessonContext';
 
 const Category = () => {
   const navigate = useNavigate();
@@ -17,28 +20,36 @@ const Category = () => {
     });
   }, []);
 
-  const handleClick = (quiz_id:number) => {
-    attemptQuiz(quiz_id, token)
-    navigate('/quiz')
+  const handleClick = (quiz_id: number) => {
+    attemptQuiz(quiz_id, token);
+    navigate('/quiz');
   };
 
   return (
     <>
-      {lessons?.map((lesson: { id: number; title: string }) => {
-        return (
-          <div className="w-1/2" key={lesson.id}>
-            <div className="flex w-full justify-between items-center mb-2">
-              <div className="">{lesson.title}</div>
-              <button
-                onClick={() => handleClick(lesson.id)}
-                className="border-2 rounded px-2 py-1"
-              >
-                Start Quiz
-              </button>
+      <div className="text-xl font-bold mb-10">Categories</div>
+      <div className="flex ">
+        {lessons?.map((lesson: { id: number; title: string }) => {
+          return (
+            <div key={lesson.id}>
+              <div className="border-2 p-5 m-2 items-center mb-2 rounded">
+                <div className="font-bold text-lg">{lesson.title}</div>
+                <div className="mb-8">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Delectus ut aspernatur quae adipisci nam impedit in
+                  laboriosam, omnis earum magni.
+                </div>
+                <button
+                  onClick={() => handleClick(lesson.id)}
+                  className="border-2 rounded px-2 py-1"
+                >
+                  Start
+                </button>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
